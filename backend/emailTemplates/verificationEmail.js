26e18f7ee2cc1967a11d5e639fa0385a278a814e
1007Isha <isha6016.be21@chitkara.edu.in>
2025-02-02 12:04:39 +0530

    updated 2

--- backend/emailTemplates/verificationEmail.js
+++ backend/emailTemplates/verificationEmail.js
@@ -3,7 +3,7 @@ const verificationEmail = (name, token) => `
     <body>
       <h1>Hello ${name},</h1>
       <p>Thank you for registering. Please verify your email by clicking the link below:</p>
-      <a href="https://fusionfit-1.onrender.com/verify-email/${encodeURIComponent(token)}">
+      <a href="https://fusion-fit.vercel.app/verify-email/${encodeURIComponent(token)}">
         Verify your email
       </a>
       <p>If you did not create an account, please ignore this email.</p>
